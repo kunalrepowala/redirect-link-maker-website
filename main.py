62c00a5672edc58877d5ca39e60973d2436b05a4
kunalrepowala <kunalrepowala@gmail.com>
2025-02-11 00:20:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def video_embed(code, extra):
     <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Embedded Video</title>
+       <title>@HotEror Telegram</title>
        <!-- Content Security Policy to restrict resource loading -->
        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src https://www.1024terabox.com;">
        <!-- These meta tags tell the browser not to cache this page -->
